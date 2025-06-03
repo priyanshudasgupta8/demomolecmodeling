861355a98ed7b36ee242ce9e0737316480df4c50
vivanpande <vivanspande@gmail.com>
2025-06-03 10:41:51 -0400

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +13,7 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Shape3D;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
@@ -31,64 +33,100 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setTitle("test");
 
+        // Create a list of shapes to hold the atoms and bonds
+        ArrayList<Shape3D> shapes = new ArrayList<Shape3D>();
 
-        // create sphere and move it to center of screen
-       
-        Sphere sphere = AtomsBonds.Oxygen(500, 250);
-        sphere.translateXProperty().set(500);
-        sphere.translateYProperty().set(250);
+        // Create atoms and bonds
+        Sphere Oxy = AtomsBonds.Oxygen(500, 250);
         Cylinder bond = AtomsBonds.Bond(460, 250);
-        Sphere sphere2 = AtomsBonds.Carbon(420, 250);
-        bond.rotateProperty().set(90);
+        Sphere Carb = AtomsBonds.Carbon(420, 250);
 
+        // add everything into shapers arraylist
+        shapes.add(Oxy);
+        shapes.add(Carb);
+        shapes.add(bond);
+
+        Group g = AtomsBonds.makeProtein(new Group(), 200, 200);
+
+        /* remember: bonds have to be rotated because they are initially vertical
+        * 0 is vertical, 90 is horizontal, all intervals in between are diagonal
+        */
+        bond.rotateProperty().set(90);
 
         //Camera object
         Camera cam = new PerspectiveCamera();
 
         //group - collection of items put into stage
-        Group g = new Group();
+        // Group g = new Group();
+
+        // remeber that we can add groups to groups
+        // so we can build complex shapes by creating small groups and sticking them together
+        Group g2 = new Group();
 
         // scene - window created (?)
         Scene s = new Scene(g , 1000, 500);
         s.setFill(Color.BLANCHEDALMOND);
 
-        // how we are handling key presses here
+        // how we are handling key presses here - instead or rotating the camera, we are rotating the world
+        // and moving the group around - this is stupid code and not so efficient, but it works
         Rotate worldRotX = new Rotate(0, Rotate.X_AXIS);
         Rotate worldRotY = new Rotate(0, Rotate.Y_AXIS);
 
         g.getTransforms().addAll(worldRotY, worldRotX);
+        
 
+        // Keyboard event handler for camera controls
         stage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
             switch(event.getCode()){
+                /*  left right rotations
+                this is the rotation of the world, *not the camera* (!!!!!!!!)
+                this is fucking terrible code and is absolutely suboptimal, but it works
+                alternative is matrix transformations and we need methods knowledge to do that 
+                etaash mathamsetty couldn't figure it out so we're doing it this way
+                */
                 case LEFT:
-                    worldRotY.setAngle(worldRotY.getAngle() + 10);
+                    worldRotY.setAngle(worldRotY.getAngle() + 2);
                     break;
                 case RIGHT:
-                    worldRotY.setAngle(worldRotY.getAngle() - 10);
+                    worldRotY.setAngle(worldRotY.getAngle() - 2);
                     break;
+                // up down rotations
                 case UP:
-                    worldRotX.setAngle(worldRotX.getAngle() + 10);
+                    worldRotX.setAngle(worldRotX.getAngle() + 2);
                     break;
                 case DOWN:
-                    worldRotX.setAngle(worldRotX.getAngle() - 10);
+                    worldRotX.setAngle(worldRotX.getAngle() - 2);
                     break;
-                case W: //w/s is for z
+                case Q: // shift/control is for z axis changes
                     g.setTranslateZ(g.getTranslateZ() + 10);
                     break;
-                case S:
+                case E:
                     g.setTranslateZ(g.getTranslateZ() - 10);
                     break;
-                case D:// a/d is x axis
+                case D:// a/d is x axis changes
                     g.setTranslateX(g.getTranslateX() + 10);
                     break;
                 case A:
                     g.setTranslateX(g.getTranslateX() - 10);
                     break;
-                case CONTROL:// shift/contr is for y axis
+                case S:// w/s is for y axis changes
                     g.setTranslateY(g.getTranslateY() + 10);
                     break;
-                case SHIFT:
+                case W:
                     g.setTranslateY(g.getTranslateY() - 10);
+                    break;
+                // todo - get mouse controls to move the camera
+                // todo - add key controls to change the molecule shown
+                // todo - add overlay UI to show controls and molecule info
+                case ESCAPE: // escape key to exit
+                    System.exit(0);
+                    break;
+                case SPACE: // space key to reset the camera
+                    worldRotX.setAngle(0);
+                    worldRotY.setAngle(0);
+                    g.setTranslateX(0);
+                    g.setTranslateY(0);
+                    g.setTranslateZ(0);
                     break;
                 default:
                     break;
@@ -96,10 +134,11 @@ public class App extends Application {
         });
         s.setCamera(cam);
 
+        // General iteration code for adding atoms and bonds to groups
+            for (Shape3D shape : shapes) {
+                g.getChildren().add(shape);
+        }       
 
-        g.getChildren().add(sphere);
-        g.getChildren().add(sphere2);
-        g.getChildren().add(bond);
         stage.setScene(s);
         stage.show();
     }
